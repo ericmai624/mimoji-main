bb6ab1b9c6c2fcab72f826202d20f5fc2d1aea34
Eric Mai <enmai1988@gmail.com>
2018-04-02 06:57:29 -0700

    removed main.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { store } from './stores';
+import 'normalize.css';
 
 ReactDOM.render(
   <Provider store={store}>
